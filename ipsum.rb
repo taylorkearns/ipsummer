606543ae1ef81fadb826ba70b9b17a20507e1f89
Taylor Kearns <tkearns@gmail.com>
2013-04-28 17:26:03 -0400

    Change Ipsum methods into class methods
    In order to chain them?

--- ipsum.rb
+++ ipsum.rb
@@ -2,23 +2,22 @@ require_relative 'haml_paragraph.rb'
 require_relative 'html_paragraph.rb'
 
 class Ipsum
-  def paragraph(options={})
-    Paragraph.new(options).paragraph
-  end
-  alias :p :paragraph
+  class << self
+    def paragraph(options={})
+      Paragraph.new(options).paragraph
+    end
+    alias :p :paragraph
 
-  def html_paragraph(options={})
-    HtmlParagraph.new(options).html
-  end
-  alias :htmlp :html_paragraph
+    def html_paragraph(options={})
+      HtmlParagraph.new(options).html
+    end
+    alias :html_p :html_paragraph
 
-  def haml_paragraph(options={})
-    HamlParagraph.new(options).haml
+    def haml_paragraph(options={})
+      HamlParagraph.new(options).haml
+    end
+    alias :haml_p :haml_paragraph
   end
-  alias :hamlp :haml_paragraph
 end
 
-ipsum = Ipsum.new
-p = ipsum.p
-hamlp = ipsum.hamlp
-htmlp = ipsum.htmlp
+puts = Ipsum.html_paragraph links: true
