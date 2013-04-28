9f29c60235e60586524aa761372efd9664266681
Taylor Kearns <tkearns@gmail.com>
2013-04-28 17:29:22 -0400

    Change Ipsum class methods into instance methods

--- ipsum.rb
+++ ipsum.rb
@@ -2,22 +2,18 @@ require_relative 'haml_paragraph.rb'
 require_relative 'html_paragraph.rb'
 
 class Ipsum
-  class << self
-    def paragraph(options={})
-      Paragraph.new(options).paragraph
-    end
-    alias :p :paragraph
-
-    def html_paragraph(options={})
-      HtmlParagraph.new(options).html
-    end
-    alias :html_p :html_paragraph
-
-    def haml_paragraph(options={})
-      HamlParagraph.new(options).haml
-    end
-    alias :haml_p :haml_paragraph
+  def paragraph(options={})
+    Paragraph.new(options).paragraph
   end
-end
+  alias :p :paragraph
 
-puts = Ipsum.html_paragraph links: true
+  def html_paragraph(options={})
+    HtmlParagraph.new(options).html
+  end
+  alias :html_p :html_paragraph
+
+  def haml_paragraph(options={})
+    HamlParagraph.new(options).haml
+  end
+  alias :haml_p :haml_paragraph
+end
