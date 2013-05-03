34df95e896bd574dc9b8d9caa45f0664deba525f
Taylor Kearns <tkearns@gmail.com>
2013-05-02 22:33:28 -0400

    WIP

--- html_paragraph.rb
+++ html_paragraph.rb
@@ -1,68 +1,69 @@
 require_relative 'paragraph.rb'
 
 class HtmlParagraph
-  attr_accessor :paragraph, :links
+  include Paragraph
 
   def initialize(options={})
-    @paragraph = Paragraph.new(min_sentences: options[:min_sentences], max_sentences: options[:max_sentences]).paragraph
+    @min_sentences = options[:min_sentences] || 5
+    @max_sentences = options[:max_sentences] || 10
     @links = options[:links]
-  end
-
-  def complete_paragraph
-    return linked_paragraph if links?
-    plain_paragraph
-  end
-
-  private
-
-  def links?
-    links || false
-  end
-
-  def plain_paragraph
-    "<p>#{paragraph}</p>"
-  end
-
-  def linked_paragraph
-    linked_paragraph = phrases.map { |phrase| phrase_with_link phrase }.join(' ').gsub(/\w\z/, 't.')
-    wrapped_paragraph(linked_paragraph)
   end
 
   def wrapped_paragraph(p)
     "<p>#{p}</p>"
   end
 
-  def phrase_with_link(phrase)
-    words = phrase.split
-    position = rand words.count
-    length = [1, 2, 3].sample
-    selected_words = words[position, length].join(' ').gsub(/[\.,\?!]/, '')
-    words[position, length] = linked_words(selected_words)
-    words.join(' ')
-  end
+  private
 
   def linked_words(w)
     "<a>#{w}</a>"
   end
 
-  def phrases
-    phrases = []
-    remaining_words = words
-    link_count.times do
-      phrases << remaining_words.slice!(0, break_point)
-    end
-    phrases.map { |phrase| phrase.join(' ') }
-  end
+  #def plain_paragraph
+    #"<p>#{paragraph}</p>"
+  #end
 
-  def break_point
-    words.count/link_count
-  end
+  #def complete_paragraph
+    #return linked_paragraph if links?
+    #plain_paragraph
+  #end
 
-  def link_count
-    link_count = (words.count/20).ceil
-  end
+  #def links?
+    #links || false
+  #end
 
-  def words
-    paragraph.split
-  end
+  #def linked_paragraph
+    #linked_paragraph = phrases.map { |phrase| phrase_with_link phrase }.join(' ').gsub(/\w\z/, 't.')
+    #wrapped_paragraph(linked_paragraph)
+  #end
+
+  #def phrase_with_link(phrase)
+    #words = phrase.split
+    #position = rand words.count
+    #length = [1, 2, 3].sample
+    #selected_words = words[position, length].join(' ').gsub(/[\.,\?!]/, '')
+    #words[position, length] = linked_words(selected_words)
+    #words.join(' ')
+  #end
+
+  #def phrases
+    #phrases = []
+    #remaining_words = words
+    #link_count.times do
+      #phrases << remaining_words.slice!(0, break_point)
+    #end
+    #phrases.map { |phrase| phrase.join(' ') }
+  #end
+
+  #def break_point
+    #words.count/link_count
+  #end
+
+  #def link_count
+    #link_count = (words.count/20).ceil
+  #end
+
+  #def words
+    #paragraph.split
+  #end
 end
--- paragraph.rb
+++ paragraph.rb
@@ -1,14 +1,59 @@
 require_relative 'text.rb'
 
-class Paragraph
-  attr_reader :min_sentences, :max_sentences, :text
+module Paragraph
+  attr_reader :min_sentences, :max_sentences, :text, :links
 
-  def initialize(options={})
-    @min_sentences = options[:min_sentences] || 5
-    @max_sentences = options[:max_sentences] || 10
+  def wrapped_paragraph
+    paragraph
   end
 
+  private
+
   def paragraph
+    return linked_paragraph if links?
+    plain_paragraph
+  end
+
+  def linked_paragraph
+    linked_paragraph = phrases.map { |phrase| phrase_with_link phrase }.join(' ').gsub(/\w\z/, 't.')
+    wrapped_paragraph(linked_paragraph)
+  end
+
+  def phrases
+    phrases = []
+    remaining_words = words
+    link_count.times do
+      phrases << remaining_words.slice!(0, break_point)
+    end
+    phrases.map { |phrase| phrase.join(' ') }
+  end
+
+  def phrase_with_link(phrase)
+    phrase_words = phrase.split
+    position = rand phrase_words.count
+    length = [1, 2, 3].sample
+    selected_words = phrase_words[position, length].join(' ').gsub(/[\.,\?!]/, '')
+    phrase_words[position, length] = linked_words(selected_words)
+    phrase_words.join(' ')
+  end
+
+  def break_point
+    words.count/link_count
+  end
+
+  def link_count
+    link_count = (words.count/20).ceil
+  end
+
+  def words
+    paragraph.split
+  end
+
+  def links?
+    links || false
+  end
+
+  def initial_paragraph
     sentences.split(/\.\s/).first(length).join('. ') + '.'
   end
 
@@ -16,9 +61,19 @@ class Paragraph
     rand min_sentences..max_sentences
   end
 
-  private
-
   def sentences
     Text.new.sentences
+  end
+
+  def plain_paragraph
+    initial_paragraph
+  end
+
+  def wrapped_paragraph(p)
+    p
+  end
+
+  def linked_words(w)
+    w
   end
 end
