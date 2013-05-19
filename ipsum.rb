e77e365c7136cbcfe842b817ad9a7013f7142f50
Taylor Kearns <tkearns@gmail.com>
2013-05-19 14:35:09 -0400

    Use class methods in Ipsum class
    Also remove executable testing code.

--- ipsum.rb
+++ ipsum.rb
@@ -5,7 +5,7 @@ require_relative 'html_paragraph.rb'
 require_relative 'headline.rb'
 
 class Ipsum
-  def text_paragraph(options={})
+  def self.text_paragraph(options={})
     paragraph = Paragraph.new(
                   min_sentences: options[:min_sentences],
                   max_sentences: options[:max_sentences])
@@ -13,7 +13,7 @@ class Ipsum
     TextParagraph.new(paragraph: paragraph)
   end
 
-  def html_paragraph(options={})
+  def self.html_paragraph(options={})
     paragraph = Paragraph.new(
                   min_sentences: options[:min_sentences],
                   max_sentences: options[:max_sentences])
@@ -22,7 +22,7 @@ class Ipsum
     HtmlParagraph.new(paragraph: paragraph, links: links)
   end
 
-  def haml_paragraph(options={})
+  def self.haml_paragraph(options={})
     paragraph = Paragraph.new(
                   min_sentences: options[:min_sentences],
                   max_sentences: options[:max_sentences])
@@ -31,28 +31,7 @@ class Ipsum
     HamlParagraph.new(paragraph: paragraph, links: links)
   end
 
-  def headline(options={})
+  def self.headline(options={})
     Headline.new(options).complete_text
   end
-
-  private
-
-  def paragraph
-    Paragraph.new(
-      min_sentences: options[:min_sentences],
-      max_sentences: options[:max_sentences])
-  end
 end
-
-ipsum = Ipsum.new
-ipsum.text_paragraph
-ipsum.text_paragraph min_sentences: 3, max_sentences: 4
-
-ipsum.html_paragraph
-ipsum.html_paragraph links: true, min_sentences: 3, max_sentences: 4
-
-ipsum.haml_paragraph
-ipsum.haml_paragraph min_sentences: 3, max_sentences: 4, links: true
-
-ipsum.headline min_words: 3, max_words: 4
-ipsum.headline period: true
