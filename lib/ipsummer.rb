26d5f09eae7b550ea92ceeb36fd8634393c97092
Taylor Kearns <tkearns@gmail.com>
2013-06-01 07:58:47 -0400

    Factor out get_paragraph method on Ipsummer module

--- lib/ipsummer.rb
+++ lib/ipsummer.rb
@@ -2,26 +2,27 @@ require_relative 'ipsummer/paragraph.rb'
 require_relative 'ipsummer/paragraph_text.rb'
 require_relative 'ipsummer/headline.rb'
 
-class Ipsummer
+module Ipsummer
   def self.paragraph(options={})
-    paragraph_text = ParagraphText.new(
-                  min_sentences: options[:min_sentences],
-                  max_sentences: options[:max_sentences])
-    links = options[:links]
-
-    Paragraph.new(paragraph_text: paragraph_text, links: links).unwrapped_paragraph
+    get_paragraph options, :unwrapped_paragraph
   end
 
-  def self.rails_paragraph(options={})
-    paragraph_text = ParagraphText.new(
-                  min_sentences: options[:min_sentences],
-                  max_sentences: options[:max_sentences])
-    links = options[:links]
-
-    Paragraph.new(paragraph_text: paragraph_text, links: links).rails_paragraph
+  def self.wrapped_paragraph(options={})
+    get_paragraph options, :wrapped_paragraph
   end
 
   def self.headline(options={})
     Headline.new(options).complete_text
+  end
+
+  private
+
+  def self.get_paragraph(options, method)
+    paragraph_text = ParagraphText.new(
+                  min_sentences: options[:min_sentences],
+                  max_sentences: options[:max_sentences])
+    links = options[:links]
+
+    Paragraph.new(paragraph_text: paragraph_text, links: links).send method
   end
 end
